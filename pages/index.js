5bc662b9a076848f730cab3fff8b3aab4c76f714
Abhijeet Varma <41261203+abhivarma07@users.noreply.github.com>
2023-04-18 22:53:50 +0530

    changed imports for pages

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import { lazy } from "react";
 import { Footer, Navbar } from "../components";
+import {
+  About,
+  Feedback,
+  GetStarted,
+  Insights,
+  WhatsNew,
+  World,
+} from "../sections";
 
 import Model from "../sections/Hero2";
 
 const Explore = lazy(() => import("../sections/Explore"));
-const About = lazy(() => import("../sections/About"));
-const Feedback = lazy(() => import("../sections/Feedback"));
-const GetStarted = lazy(() => import("../sections/GetStarted"));
-const Insights = lazy(() => import("../sections/Insights"));
-const WhatsNew = lazy(() => import("../sections/WhatsNew"));
-const World = lazy(() => import("../sections/World"));
+// const About = lazy(() => import("../sections/About"));
+// const Feedback = lazy(() => import("../sections/Feedback"));
+// const GetStarted = lazy(() => import("../sections/GetStarted"));
+// const Insights = lazy(() => import("../sections/Insights"));
+// const WhatsNew = lazy(() => import("../sections/WhatsNew"));
+// const World = lazy(() => import("../sections/World"));
 
 const Home = () => (
   <div className="bg-primary-black" style={{ overflow: "hidden" }}>
